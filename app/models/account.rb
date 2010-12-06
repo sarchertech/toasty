200ce6eb86e807cb6515429b2dd331522ac06e38
Seth Archer Brown <learc83@Seth-Browns-MacBook-Pro.local>
2010-12-06 04:11:58 -0500

    added validations and tests for account.sub_domain

--- app/models/account.rb
+++ app/models/account.rb
@@ -10,19 +10,24 @@ class Account < ActiveRecord::Base
   validates_inclusion_of :customer_location_access, :in => [true, false]
   validates_inclusion_of :user_location_access, :in => [true, false]
 
-  validates_presence_of :account_number
+  validates_presence_of :account_number, :name
   
   validates_format_of :name, :without => /[^a-zA-Z0-9\s]/, 
                       :message => "must contain only letters, numbers,
                                    and spaces"
 
+  validates_uniqueness_of :name, :case_sensitive => false
+  validates_uniqueness_of :sub_domain, :case_sensitive => false,
+                          :message => "is already taken (your account name is
+                                       not unique - please change it)"
+
   private
   
   def strip_whitespace
-    self.name = name.strip
+    self.name = name.strip.gsub(/ {2,}/,' ') if self.name
   end
 
   def create_sub_domain
-    self.sub_domain = url_sanitize(name)
+    self.sub_domain = url_sanitize(name) if self.name
   end
 end
--- test/unit/account_test.rb
+++ test/unit/account_test.rb
@@ -41,9 +41,43 @@ class AccountTest < ActiveSupport::TestCase
     assert !account.valid?
   end
 
+  test "name should not be blank" do
+    account = Factory.build(:account, :name => nil)
+
+    assert !account.valid?
+  end
+
   test "name should be alphanumeric " do 
     account = Factory.build(:account, :name => "Sun City's!")
 
     assert !account.valid?
+  end
+
+  test "name should be unique" do
+    account = Factory.create(:account, :name => "Sun City")
+    account2 = Factory.build(:account, :name => "Sun City")
+
+    assert !account2.valid? 
+  end
+
+  test "name uniqueness should not be case sensitive" do
+    account = Factory.create(:account, :name => "Sun City")
+    account2 = Factory.build(:account, :name => "sun city")
+
+    assert !account2.valid?
+  end
+
+  test "extra spaces should not make name unique" do
+    account = Factory.create(:account, :name => "Sun City")
+    account2 = Factory.build(:account, :name => "Sun  City")
+
+    assert !account2.valid?
+  end
+
+  test "sub_domain should be unique" do
+    account = Factory.create(:account, :name => "Sun City")
+    account2 = Factory.build(:account, :name => "Suncity")
+
+    assert !account2.valid?
   end
 end

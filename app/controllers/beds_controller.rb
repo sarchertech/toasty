c57484808ac307404471a0acbf90ecde360f3c63
Seth Archer Brown <learc83@Seth-Browns-MacBook-Pro.local>
2010-12-18 16:33:41 -0500

    finished beds controller and tests

--- app/controllers/beds_controller.rb
+++ app/controllers/beds_controller.rb
@@ -1,10 +1,60 @@
 class BedsController < ApplicationController
-  # GET /beds
+  # GET /salons/1/beds
   def index
     @beds = @current_salon.beds 
   end
 
+  # GET /salons/1/beds/1
   def show
     @bed = @current_salon.beds.find(params[:id])
+  end
+
+  # GET /salons/1/beds/new
+  def new
+    @bed = Bed.new
+  end
+
+  # GET /salons/1/beds/1/edit
+  def edit
+    @bed = @current_salon.beds.find(params[:id])
+  end
+
+  # POST /salons/1/beds
+  def create
+    @bed = Bed.new(params[:bed])
+    @bed.salon_id = @current_salon.id
+
+    respond_to do |format|
+      if @bed.save
+        format.html {redirect_to([@current_salon, @bed],
+                       :notice => "Bed was successfully created")}
+      else
+        format.html {render :action => "new"}
+      end
+    end
+  end
+
+  # PUT /salons/1/beds/1
+  def update
+    @bed = @current_salon.beds.find(params[:id])
+
+    respond_to do |format|
+      if @bed.update_attributes(params[:bed])
+        format.html {redirect_to([@current_salon, @bed],
+                       :notice => "Bed was successfully updated")}
+      else
+        format.html {render :action => "edit"}
+      end
+    end
+  end
+
+  #DELETE /salons/1/beds/1
+  def destroy
+    @bed = @current_salon.beds.find(params[:id])
+    @bed.destroy
+
+    respond_to do |format|
+      format.html {redirect_to(salon_beds_url)}
+    end
   end
 end
--- test/functional/beds_controller_test.rb
+++ test/functional/beds_controller_test.rb
@@ -34,4 +34,70 @@ class BedsControllerTest < ActionController::TestCase
       get :show, :salon_id => @salon.to_param, :id => @bed2.to_param
     end
   end
+
+  test "should get new" do
+    get :new, :salon_id => @salon.to_param
+    assert_response :success
+    assert assigns(:bed).new_record?
+  end
+
+  test "should get edit and only assign a bed scoped to current_salon" do
+    get :edit, :salon_id => @salon.to_param, :id => @bed.to_param
+    assert_response :success
+    assert_equal(@bed, assigns(:bed))
+
+    assert_raises(ActiveRecord::RecordNotFound) do
+      get :edit, :salon_id => @salon.to_param, :id => @bed2.to_param
+    end    
+  end
+
+  test "should create bed scoped to current_account & redirect" do
+    assert_difference('Bed.count') do
+      post :create, :salon_id => @salon.to_param, 
+           :bed => Factory.attributes_for(:bed, :salon_id => nil)
+    end
+
+    assert_equal(@salon.id, assigns(:bed).salon_id)
+
+    assert_redirected_to salon_bed_path(@salon, assigns(:bed))
+  end
+
+  test "should render new if bed not successfully created" do
+    post :create, :salon_id => @salon.to_param, 
+         :bed => Factory.attributes_for(:bed, :level => nil)
+
+    assert_template("new")
+  end
+
+  test "should update bed scoped to salon and redirect" do
+    put :update, :salon_id => @salon.to_param, :id => @bed.to_param,
+        :bed => {:level => "2"}
+
+    assert_equal(2, @bed.reload.level)
+
+    assert_redirected_to salon_bed_path(@salon, @bed)
+
+    assert_raises(ActiveRecord::RecordNotFound) do
+      put :update, :salon_id => @salon.to_param, :id => @bed2.to_param
+    end
+  end
+
+  test "should render edit if bed not successfully updated" do
+    put :update, :salon_id => @salon.to_param, :id => @bed.to_param,
+                 :bed => {:level => nil}
+
+    assert_template("edit")
+  end
+
+  test "should destroy bed scoped to salon" do
+    assert_difference("Bed.count", -1) do
+      delete :destroy, :salon_id => @salon.to_param, :id => @bed.to_param
+    end
+
+    assert_redirected_to salon_beds_path
+
+    assert_raises(ActiveRecord::RecordNotFound) do
+      delete :destroy, :salon_id => @salon.to_param, :id => @bed2.to_param
+    end
+  end
 end

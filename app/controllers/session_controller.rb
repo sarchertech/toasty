9dc25507283a97e6ed842a931633a8c404196acf
Seth Archer Brown <learc83@Seth-Browns-MacBook-Pro.local>
2011-01-05 16:39:14 -0500

    login form and flash_handler helper method

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,6 +1,6 @@
 class SessionController < ApplicationController
   # GET /login
-  def index
+  def new
   end
 
   # POST /login
@@ -8,18 +8,19 @@ class SessionController < ApplicationController
     @user = @current_account.users.find_by_login(params[:login])
 
     if @user && @user.too_many_tries?
-      err = "You have attempted to log in with an incorrect password too many
-             times. You must #{@user.how_long} before you can login."
+      flash.now[:alert] = "You have attempted to log in with an incorrect 
+                           password too many times. You must #{@user.how_long} 
+                           before you can login."
       
-      render :action => "new", :error => err
+      render :action => "new"
     elsif @user && @user.has_password?(params[:password])
       session[:user_id] = @user.id
       
       redirect_to("/")
     else
-      err = "login/password combination does not match"
+      flash.now[:alert] = "login/password combination does not match"
       
-      render :action => "new", :error => err    
+      render :action => "new"  
     end
   end
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def flash_handler
+    if flash[:notice]
+      %Q{<p class="notice">#{flash[:notice]}</p>'}.html_safe
+    elsif flash[:alert]
+      %Q{<p class="alert">#{flash[:alert]}</p>}.html_safe
+    end
+  end
 end

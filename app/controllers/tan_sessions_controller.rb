fae8f1d208319d92f0e4add05ffe927298ae8d79
Seth Archer Brown <learc83@Seth-Browns-MacBook-Pro.local>
2011-01-21 00:48:46 -0500

    working on dashboard

--- app/controllers/tan_sessions_controller.rb
+++ app/controllers/tan_sessions_controller.rb
@@ -1,2 +1,7 @@
 class TanSessionsController < ApplicationController
+  # GET /salons/:salon_id/tan_sessions/new
+  def new
+    @beds = @current_salon.beds
+    @tan_session = TanSession.new
+  end
 end
--- public/javascripts/sessions.js
+++ public/javascripts/sessions.js
@@ -1,3 +1,7 @@
 $(document).ready(function() {
-
+  $("#new_tan_session").submit(function() {
+    var form = $(this)
+    var url = "http://localhost:3500"
+    return false;
+  });
 });

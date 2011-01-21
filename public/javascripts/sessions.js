6f4c80911b40819cd32030ae8a929c61c43acda9
Michael Krontz <mkrontz@mac.com>
2011-01-21 18:54:11 -0500

    worked on bed control javascript

--- public/javascripts/sessions.js
+++ public/javascripts/sessions.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
   $("#new_tan_session").submit(function() {
     var form = $(this)
-    var url = "http://localhost:3500"
+    var url = "http://localhost:3333/"
+	var minutes = $("#tan_session_minutes").val();
+	$.get(url + minutes);
     return false;
   });
 });

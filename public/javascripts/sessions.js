fe81c071adccef39d5aa683831a9ce1319d835d7
Seth Archer Brown <learc83@seth-pc-mac.(none)>
2011-01-27 17:36:40 -0500

    worked on dashboard

--- public/javascripts/sessions.js
+++ public/javascripts/sessions.js
@@ -2,17 +2,52 @@ function isTmaxLocal() {
 }
 
 function getTimeStatus(beds) {
-	//alert("foo")
+	var url = "http://localhost:4567/2/"
+	$.ajax({
+	  url: url + beds,
+	  dataType: 'json',
+	  success: function(json) {
+	    applyTimeStatus(json);
+	  },
+	  error: function(xhr, ajaxOptions, thrownError){
+	    alert('status and times error--' + thrownError);
+	  }
+	});
+};
+
+function applyTimeStatus(json) {
+  $.each(json, function(i, val) {
+    $("#_" + val.number).removeClass().addClass("_" + val.status);
+    if (val.time) {
+      $("#_" + val.number + " .countdown").html(minutes(val.time));
+    };
+  });
+};
+
+function minutes(seconds) {
+  var minutes = Math.floor(seconds/60);
+  var seconds = seconds % 60;
+  seconds = ( seconds < 10 ? "0" : "" ) + seconds
+  return minutes + ":" + seconds;
 };
 
 $(document).ready(function() {
   $("#new_tan_session").submit(function() {
     var form = $(this)
-    var url = "http://localhost:3333/1/"
+    var url = "http://localhost:4567/1/"
 		var bed = $("#tan_session_bed_id").val();
 		var minutes = $("#tan_session_minutes").val();
 		var delay = $("#tan_session_delay").val();
-		$.get(url + bed + "/" + minutes + "/" + delay);
+		//$.get(url + bed + "/" + minutes + "/" + delay);
+		$.ajax({
+		  url: url + bed + "/" + minutes + "/" + delay,
+		  success: function() {
+		    alert('bed ' + bed + ' activated');
+		  },
+		  error: function(xhr, ajaxOptions, thrownError){
+		    alert('bed not activated--' + thrownError );
+		  }
+		});
     return false;
   });
 
@@ -23,5 +58,5 @@ $(document).ready(function() {
 		return false;
 	});
 	
-	getTimeStatus(3);
+	getTimeStatus(14);
 });

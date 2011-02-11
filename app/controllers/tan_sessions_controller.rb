0facb91e9d062a4d8db8dfd94be7a7c48ed39f2c
Seth Archer Brown <learc83@Seth-Browns-MacBook-Pro.local>
2011-02-11 18:58:58 -0500

    changed bed order

--- app/controllers/tan_sessions_controller.rb
+++ app/controllers/tan_sessions_controller.rb
@@ -1,7 +1,7 @@
 class TanSessionsController < ApplicationController
   # GET /salons/:salon_id/tan_sessions/new
   def new
-    @beds = @current_salon.beds
+    @beds = @current_salon.beds.order("bed_number ASC")
     @tan_session = TanSession.new
   end
 end

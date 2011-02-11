c6a26a45816558f76aca642d07adcec4da3614c4
Seth Archer Brown <learc83@Seth-Browns-MacBook-Pro.local>
2011-02-11 18:53:50 -0500

    changed bed ordering to descending

--- app/controllers/customer_tan_sessions_controller.rb
+++ app/controllers/customer_tan_sessions_controller.rb
@@ -1,7 +1,7 @@
 class CustomerTanSessionsController < ApplicationController
   # GET /salons/:salon_id/tan_sessions/new
   def new
-    @beds = @current_salon.beds
+    @beds = @current_salon.beds.order("bed_number DESC")
     @tan_session = TanSession.new
     @customer = Customer.find(session[:customer_id])
   end

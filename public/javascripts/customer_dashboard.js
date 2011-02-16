d02f94dfdb2fd2c08ec0c2f919571a3f5a3db10d
Seth Archer Brown <learc83@Seth-Browns-MacBook-Pro.local>
2011-02-15 23:00:21 -0500

    javascript timeout bug

--- public/javascripts/customer_dashboard.js
+++ public/javascripts/customer_dashboard.js
@@ -193,10 +193,10 @@ $(document).ready(function() {
     };
 	});
 
-  var idleTimer = window.setTimeout(doTimeout, 60000);
+  var idleTimer = window.setTimeout(doTimeout, 10000);
   
-  $(this).mousemove(function(e){
-    window.clearTimeout(idleTimer);
-    idleTimer = window.setTimeout(doTimeout, 60000);
-  });
+  //$(this).mousemove(function(e){
+    //window.clearTimeout(idleTimer);
+    //idleTimer = window.setTimeout(doTimeout, 60000);
+  //});
 });
